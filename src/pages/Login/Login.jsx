3dd905bebbc69d3cf791479c9d2d0b2072266b7a
anubhavi-hoon <anshjaiswal127@gmail.com>
2026-05-05 16:42:12 +0530

    Updated:Login Page Style

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,29 +5,35 @@ import { LuLock } from "react-icons/lu";
 
 const Login = () => {
   return (
-    <div className='wrapper'>
-        <form action="">
-            <h1>Login</h1>
-            <div className='inp-box'>
-                <input type="text" 
-                placeholder='Username' required/>
-                <FaUser className='icon '/>
-            </div>
+    <div className='login-container'>
+      <div className='wrapper'>
+          <form action="">
+              <h1>Login</h1>
+              
               <div className='inp-box'>
-                <input type="password" 
-                placeholder='Password' required/>
-                <LuLock className='icon' />
-            </div>
-            <div className="remember-forgot">
-                <label ><input type="Checkbox" />Remember Me</label>
-                <a href="#">Forgot Password</a>
-
-            </div>
-            <button type='submit'>Login</button>
-            <div className="register-link">
-                <p>Dont have an account? <a href="#">Register</a></p>
-            </div>
-        </form>
+                  <input type="text" placeholder='Username' required />
+                  <FaUser className='icon' />
+              </div>
+              
+              <div className='inp-box'>
+                  <input type="password" placeholder='Password' required />
+                  <LuLock className='icon' />
+              </div>
+              
+              <div className="remember-forgot">
+                  <label>
+                      <input type="checkbox" /> Remember me
+                  </label>
+                  <a href="#">Forgot password?</a>
+              </div>
+              
+              <button type='submit'>Login</button>
+              
+              <div className="register-link">
+                  <p>Don't have an account? <a href="#">Register</a></p>
+              </div>
+          </form>
+      </div>
     </div>
   )
 }
